5a6c7a02c66228f9d2d263e9160c08790ddaebde
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-12-03 15:30:43 -0300

    Adicionado função de adicionar numa posição dada em listas duplamentes encadeadas

--- ListasEncadeadas/ListasDuplamenteEncadeadas/list.py
+++ ListasEncadeadas/ListasDuplamenteEncadeadas/list.py
@@ -27,11 +27,33 @@ class DoubleLinkedList():
         newNode.data = data
 
         if(self.head == None):
-            self.head = newNode
+            self.head = self.tail = newNode
         else:
             current = self.head
             while current.next != None:
                 current = current.next
             newNode.prev = current
             newNode.next = None
+            self.tail = newNode
         self.length += 1
+    
+    def insertAtPosition(self, data, positionToInsert):
+        newNode = Node()
+        newNode.data = data
+        
+        if positionToInsert < 0 or positionToInsert > self.length:
+            return
+        elif self.head == None or positionToInsert == 0:
+            self.insertAtBeginning()
+        elif positionToInsert == self.length:
+            self.insertAtEnd()
+        else:
+            current = self.head
+            count = 0
+            while count < positionToInsert and current.next != None:
+                current = current.next
+                count += 1
+            newNode.next = current.next
+            newNode.prev = current
+            current.next = newNode
+            self.length += 1
