c4c927d95fb3b2562bfa639a7a696cb68259fd40
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-12-03 20:11:29 -0300

    Terminando de implementar Pilha com Array e testando-a

--- Pilhas/PilhaComArray/pilha.py
+++ Pilhas/PilhaComArray/pilha.py
@@ -1,11 +1,11 @@
 class Stack:
     def __init__(self, Capacity=1):
-        self.top = 0
+        self.top = -1
         self.Capacity = Capacity
         self.A = [None]*Capacity
     
     def push(self, data):
-        if self.top == self.Capacity:
+        if self.top == self.Capacity - 1:
             print("Stack Overflow")
             return
         self.top += 1
@@ -16,6 +16,7 @@ class Stack:
             print("Stack Underflox")
             return
         valueInStackPosition = self.A[self.top]
+        self.A[self.top] = None
         self.top -= 1
         return valueInStackPosition
     
@@ -29,4 +30,8 @@ class Stack:
         return self.top == -1
     
     def isFull(self):
-        return self.Capacity == self.top
+        return self.top == self.Capacity-1
+    
+    def printStack(self):
+        for item in self.A:
+            print(item) 
---
+++ Pilhas/PilhaComArray/test.py
@@ -0,0 +1,11 @@
+from pilha import Stack
+
+pilha = Stack(5)
+pilha.push(1)
+pilha.push(2)
+pilha.push(3)
+pilha.push(4)
+pilha.push(5)
+pilha.push(6)
+pilha.printStack()
+print(pilha.isFull())
