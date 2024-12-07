ba962b8536656b34d7eba0d6f46c770c468a062f
Marcos Iury <MarcosIuryProgramador@gmail.com>
2024-12-07 14:42:05 -0300

    Adicionando impressão com pós-ordem

--- Arvores/Tree.py
+++ Arvores/Tree.py
@@ -12,39 +12,51 @@ class BinaryTree:
         else:
             self.root = None
     
-    # percurso em ordem simétrica
+    # percurso em ordem simétrica (inorder)
     def simetric_traversal(self, node=None):
         if node is None:
             node = self.root
         if node.left:
-            print('(', end='')
+            print('(', end='') # Parênteses específicos para o nosso exemplo.
             self.simetric_traversal(node.left)
         print(node.data, end='')
         if node.right:
             self.simetric_traversal(node.right)
             print(')', end='')
+    
+    def postorder_traversel(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.postorder_traversel(node.left)
+        if node.right:
+            self.postorder_traversel(node.right)
+        print(node.data)
+        
 
 
 if __name__ == "__main__":
     tree = BinaryTree()
-    n1 = Node('a')
-    n2 = Node('+')
-    n3 = Node('*')
-    n4 = Node('b')
-    n5 = Node('-')
-    n6 = Node('/')
-    n7 = Node('c')
-    n8 = Node('d')
-    n9 = Node('e')
+    n1 = Node('I')
+    n2 = Node('N')
+    n3 = Node('S')
+    n4 = Node('C')
+    n5 = Node('R')
+    n6 = Node('E')
+    n7 = Node('V')
+    n8 = Node('A')
+    n9 = Node('5')
+    n10 = Node('3')
 
-    n6.left = n7
-    n6.right = n8
-    n5.left = n6
-    n5.right = n9
-    n3.left = n4
-    n3.right = n5
-    n2.left = n1
-    n2.right = n3
+    n10.left = n6
+    n10.right = n9
+    n6.left = n1
+    n6.right = n5
+    n5.left = n2
+    n5.right = n4
+    n4.left = n3
+    n9.left = n8
+    n8.right = n7
 
-    tree.root = n2
-    tree.simetric_traversal()
+    tree.root = n10
+    tree.postorder_traversel()
