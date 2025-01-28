3632526dcf95c3af52b60710171bc762596a6bf4
Marcos Iury <MarcosIuryProgramador@gmail.com>
2025-01-28 13:27:39 -0300

    Implementado algoritmo de compressão por dobra

--- HashMaps/AtividadeHashMaps/main.py
+++ HashMaps/AtividadeHashMaps/main.py
@@ -37,9 +37,26 @@ def compression_division(map):
         map[key%32] = value
     return map
 
+# Compressão por dobra
+def compression_fold(map):
+    for key in list(map.keys()):
+        keyValue = map.pop(key)
+        stringKey = str(key)
+        while len(stringKey) % 4 != 0:
+            stringKey += "0"
+        while len(stringKey) > 2:
+            compressed_key = ""
+            for i in range(0, len(stringKey), 4):
+                sum1 = int(stringKey[i]) + int(stringKey[i+3])
+                sum2 = int(stringKey[i+1]) + int(stringKey[i+2])
+                compressed_key += str(sum1 % 10) + str(sum2 % 10)
+            stringKey = compressed_key
+        key = int(stringKey)
+        map[key%32] = keyValue
+    return map
+
+
 # Strings utilizadas para o teste
-
-
 if __name__ == "__main__":
     strings = [ "apple", "voadora", "banjo", "banana", "cherry", "date",
     "elderberry", "fig", "grape", "honeydew", "kiwi", "xuru", "runin", "xamã",
@@ -49,3 +66,6 @@ if __name__ == "__main__":
     map = {}
     for string in strings:
         map[hash_sum(string)] = string
+    print(map)
+    print("---------")
+    print(compression_fold(map))
